ebfbf57c5bf4e2bdeddcaadec83e92a129237223
OprahBASE <oprahlewis4@gmail.com>
2024-02-01 17:07:03 -0500

    Updated PROD URL in quasar.config.js

--- client/quasar.config.js
+++ client/quasar.config.js
@@ -12,7 +12,7 @@
 const { configure } = require('quasar/wrappers');
 
 
-module.exports = configure(function (/* ctx */) {
+module.exports = configure(function ( ctx ) {
   return {
     eslint: {
       // fix: true,
@@ -60,7 +60,15 @@ module.exports = configure(function (/* ctx */) {
         node: 'node20'
       },
 
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode:'history', // available values: 'hash', 'history'
+      env: {
+        BASE_URL: ctx.dev ? 'http://localhost:3000' : 'http://54.242.68.115>',
+        CREATOR_NAME: 'Oprah Lewis',
+        CREATOR_EMAIL: 'oprahlewis4@gmail.com',
+        LINKEDIN: 'https://www.linkedin.com/in/oprahlewis/'
+      },
+      distDir: '../server/public'
+ 
       // vueRouterBase,
       // vueDevtools,
       // vueOptionsAPI: false,
@@ -86,6 +94,11 @@ module.exports = configure(function (/* ctx */) {
     },
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#devServer
+    htmlVariables: {
+      productName: 'BAHS Student Job Survey App',
+      productDescription: 'The Bronx Aerospace High School Student Survey App collects job preferences from high school students.'
+    },
+ 
     devServer: {
       // https: true
       open: true // opens browser window automatically
@@ -106,7 +119,9 @@ module.exports = configure(function (/* ctx */) {
       // directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: [
+        'Notify'
+      ]
     },
 
     // animations: 'all', // --- includes all animations
